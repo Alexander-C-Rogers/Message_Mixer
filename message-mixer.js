79ca3c38364449eff2a645512cb7eb9b9a1d5aba
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-08 14:59:33 -0400

    encryption functions

---
+++ message-mixer.js
@@ -0,0 +1,6 @@
+// Import the functions from encryptors.js here.
+const encryptors = require('./encryptors.js');
+
+// Encryption Functions
+/////////////////////////////////////////////
+const { caesarCipher, symbolCipher, reverseCipher } = encryptors;
