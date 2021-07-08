c73585bf1378887afb0590fdefe2e8b00150c7ef
Alexander Rogers <alexander.caelan.rogers@gmail.com>
2021-07-08 15:11:08 -0400

    user input / output

--- super-encryptors.js
+++ super-encryptors.js
@@ -10,3 +10,18 @@ const decodeMessage = (str) => {
     // Use the encryptor functions here.
     return caesarCipher(symbolCipher(reverseCipher(str)), -6);
 }
+
+// User input / output.
+const handleInput = (userInput) => {
+    const str = userInput.toString().trim();
+    let output;
+    if (process.argv[2] === 'encode') {
+      output = encodeMessage(str);
+    } 
+    if (process.argv[2] === 'decode') {
+      output = decodeMessage(str);
+    } 
+
+    process.stdout.write(output + '\n');
+    process.exit();
+}
